83b8342c0d137bda543f065675f363df3b657b0d
Adamya <adamyapathak12@gmail.com>
2022-10-16 08:13:35 -0400

    Add files via upload

---
+++ BasicCoding.java
@@ -0,0 +1,23 @@
+public class BasicCoding {
+    public static void main(String[] args) throws Exception {
+int a = 8;
+int b= 2;
+int c = 4;
+int d = 6;
+int e = 20;
+int f = 80;
+int g = 40;
+int h = 60;
+int i = 100;
+System.out.println(a + i);
+System.out.println(b + c);
+System.out.println(f - e);
+System.out.println(g * h);
+System.out.println(d / c);
+System.out.println(g / e);
+System.out.println(i - 31) ;
+System.out.println("Mr.Mentor");
+System.out.println (16 / 8);
+System.out.println(g + a);
+    }
+}
